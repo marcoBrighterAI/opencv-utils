aa9db14a4c34a72627253670c1b12d497eab963a
marcoBrighterAI <marco@brighter.ai>
2021-06-05 18:59:06 +0200

    refactor: change variable names to better reflect the value

--- 07-flip/flip.py
+++ 07-flip/flip.py
@@ -7,26 +7,29 @@ import cv2
 
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", type=str, default="opencv_logo.png",
-	help="path to the input image")
+ap.add_argument("-i", "--image", type=str, default="assets/opencv_logo.png",
+                help="path to the input image")
 args = vars(ap.parse_args())
 
 # load the original input image and display it to our screen
 image = cv2.imread(args["image"])
 cv2.imshow("Original", image)
+cv2.waitKey(0)
 
 # flip the image horizontally
 print("[INFO] flipping image horizontally...")
-flipped = cv2.flip(image, 1)
-cv2.imshow("Flipped Horizontally", flipped)
+flipped_horizontally = cv2.flip(image, 1)
+cv2.imshow("Flipped Horizontally", flipped_horizontally)
+cv2.waitKey(0)
 
 # flip the image vertically
-flipped = cv2.flip(image, 0)
 print("[INFO] flipping image vertically...")
-cv2.imshow("Flipped Vertically", flipped)
+flipped_vertically = cv2.flip(image, 0)
+cv2.imshow("Flipped Vertically", flipped_vertically)
+cv2.waitKey(0)
 
 # flip the image along both axes
-flipped = cv2.flip(image, -1)
 print("[INFO] flipping image horizontally and vertically...")
-cv2.imshow("Flipped Horizontally & Vertically", flipped)
+flipped_horizontally_and_vertically = cv2.flip(image, -1)
+cv2.imshow("Flipped Horizontally & Vertically", flipped_horizontally_and_vertically)
 cv2.waitKey(0)
