20c2b7e679c52043f014c84b8137f0bf62b50aa6
marcoBrighterAI <marco@brighter.ai>
2021-06-09 21:48:38 +0200

    feat: change default input image

--- 02-image-processing-operations/03-smoothing-and-blurring/blurring.py
+++ 02-image-processing-operations/03-smoothing-and-blurring/blurring.py
@@ -7,7 +7,7 @@ import cv2
 
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", type=str, default="/assets/lena.png",
+ap.add_argument("-i", "--image", type=str, default="assets/lena.png",
                 help="path to input image")
 args = vars(ap.parse_args())
 
