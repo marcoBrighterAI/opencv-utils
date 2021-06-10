acea4b80be5f3ca0effb61f17bccc855738dfd0b
marcoBrighterAI <marco@brighter.ai>
2021-06-10 20:27:56 +0200

    style: change the format of the file to add more tabs and spaces

--- 02-image-processing-operations/06-adapative-thresholding/adaptive_thresholding.py
+++ 02-image-processing-operations/06-adapative-thresholding/adaptive_thresholding.py
@@ -8,7 +8,7 @@ import cv2
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--image", type=str, required=True,
-	help="path to input image")
+                help="path to input image")
 args = vars(ap.parse_args())
 
 # load the image and display it
@@ -21,13 +21,13 @@ blurred = cv2.GaussianBlur(gray, (7, 7), 0)
 
 # apply simple thresholding with a hardcoded threshold value
 (T, threshInv) = cv2.threshold(blurred, 230, 255,
-	cv2.THRESH_BINARY_INV)
+                               cv2.THRESH_BINARY_INV)
 cv2.imshow("Simple Thresholding", threshInv)
 cv2.waitKey(0)
 
 # apply Otsu's automatic thresholding
 (T, threshInv) = cv2.threshold(blurred, 0, 255,
-	cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)
+                               cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)
 cv2.imshow("Otsu Thresholding", threshInv)
 cv2.waitKey(0)
 
@@ -35,7 +35,7 @@ cv2.waitKey(0)
 # adaptive thresholding to examine neighborhoods of pixels and
 # adaptively threshold each neighborhood
 thresh = cv2.adaptiveThreshold(blurred, 255,
-	cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, 21, 10)
+                               cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, 21, 10)
 cv2.imshow("Mean Adaptive Thresholding", thresh)
 cv2.waitKey(0)
 
@@ -43,6 +43,6 @@ cv2.waitKey(0)
 # weighting versus a simple mean to compute our local threshold
 # value
 thresh = cv2.adaptiveThreshold(blurred, 255,
-	cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 21, 4)
+                               cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 21, 4)
 cv2.imshow("Gaussian Adaptive Thresholding", thresh)
 cv2.waitKey(0)
